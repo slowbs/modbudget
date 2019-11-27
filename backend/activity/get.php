05128c4ae7b60fd0af7532646fb688f44171cf90
slowbs <poison_word@hotmail.com>
2019-11-27 10:47:59 +0700

    activity refund

--- backend/activity/get.php
+++ backend/activity/get.php
@@ -1,9 +1,9 @@
 <?php
 if (isset($_GET['budgetno'])) {
-    $query = 'select id, a.budgetno, a.activityno, name, income, outcome, sum(income - outcome) as balance from (
+    $query = 'select id, a.budgetno, a.activityno, name, income, outcome, refund, sum(income - outcome + refund) as balance from (
         (select *  from activity) as a
         left join
-        (select activityno, sum(income) as income, sum(outcome) as outcome from list GROUP by activityno) as l on l.activityno = a.activityno)
+        (select activityno, sum(income) as income, sum(outcome) as outcome, sum(refund) as refund from list GROUP by activityno) as l on l.activityno = a.activityno)
         where budgetno = ?
         group by activityno';
     $stmt = mysqli_prepare($database, $query);
