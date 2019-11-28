f6663b63df2c852a7a2ec6afb850fcaeaa422a2e
slowbs <poison_word@hotmail.com>
2019-11-28 15:04:58 +0700

    add xlsx

--- frontend/src/app/components/report/report.component.ts
+++ frontend/src/app/components/report/report.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { BudgetService, IReport } from '../budget.service';
 import { Router } from '@angular/router';
-import { AppURL } from '../../app.url'
+import { AppURL } from '../../app.url';
+import * as XLSX from 'xlsx';  
 
 @Component({
   selector: 'app-report',
@@ -9,6 +10,15 @@ import { AppURL } from '../../app.url'
   styleUrls: ['./report.component.css']
 })
 export class ReportComponent implements OnInit {
+
+  @ViewChild('TABLE', { static: false }) TABLE: ElementRef;  
+  title = 'Excel';  
+  ExportTOExcel() {  
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(this.TABLE.nativeElement);  
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();  
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');  
+    XLSX.writeFile(wb, 'ScoreSheet.xlsx');  
+  }  
 
   AppURL = AppURL;
   public reportItem: IReport[] = [];
