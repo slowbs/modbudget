7801fe4f6cd66ab6bb84cfbda3492e66aea84ac8
slowbs <poison_word@hotmail.com>
2019-11-28 16:00:29 +0700

    deploy test lan

--- backend/configs/database.php
+++ backend/configs/database.php
@@ -1,37 +1,32 @@
 <?php
 //ปิด error
-@ini_set('display_errors', '0');
-
-/** Development */
-$host = 'localhost';
-$user = 'slowbs';
-$password = 'sodsongig4';
-$dbname = 'modbudget';
+// @ini_set('display_errors', '0');
 
 /** Production */
-if( PRODUCTION )
-{
+if (PRODUCTION) {
     $host = '61.19.202.217';
     $user = 'hdc';
     $password = 'hdcnst';
     $dbname = 'modbudget';
-}
-
-
-/** Test in 5.104 */
-elseif( TESTLAN )
-{
+} elseif (TESTLAN) {
+    /** Development */
     $host = '192.168.5.104';
+    $user = 'slowbs';
+    $password = 'sodsongig4';
+    $dbname = 'modbudget';
+} else {
+    /** Development */
+    $host = 'localhost';
     $user = 'slowbs';
     $password = 'sodsongig4';
     $dbname = 'modbudget';
 }
 
 
-$database = mysqli_connect($host,$user,$password,$dbname);
-mysqli_set_charset($database,"utf8");
+$database = mysqli_connect($host, $user, $password, $dbname);
+mysqli_set_charset($database, "utf8");
 
-if(!$database){
+if (!$database) {
     http_response_code(500);
     echo json_encode([
         'message' => 'DB cannot connect',
--- backend/configs/defines.php
+++ backend/configs/defines.php
@@ -6,5 +6,8 @@ define('PRODUCTION', $_SERVER['SERVER_NAME'] == '61.19.202.217');
 define('TESTLAN', $_SERVER['SERVER_NAME'] == '192.168.5.104');
 
 $routes = [];
-$route  = explode('?', str_replace('/backend', '', $_SERVER['REQUEST_URI']))[0];
+//เพิ่ม subfolder สำหรับ Host
+$route  = explode('?', str_replace('/modbudget/backend', '', $_SERVER['REQUEST_URI']))[0];
 $method = $_SERVER['REQUEST_METHOD'];
+
+// print_r($route);
