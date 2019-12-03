88b9333bc63f3eec056a4e81c885749764173b7e
slowbs <poison_word@hotmail.com>
2019-12-03 15:02:18 +0700

    check if Reportgroup > 1

--- backend/reportgroup/get.php
+++ backend/reportgroup/get.php
@@ -1,14 +1,28 @@
 <?php
 if (isset($_GET['id'])) {
-    $query = 'select pj.projectno, w.id, pj.name, workgroup, income, outcome, refund, sum(income - outcome + refund) as balance, person from (
+    $query = '(select pj.projectno, w.id, pj.name, workgroup, income, outcome, refund, sum(income - outcome + refund) as balance from (
         (select  projectno, sum(income) as income, sum(outcome) as outcome, sum(refund) as refund  from list GROUP by projectno) as l
         right JOIN
         (select * from project) as pj on pj.projectno = l.projectno
         left JOIN
         (select * from workgroup) as w on w.name = pj.workgroup)
-        where w.id = ? GROUP by l.projectno order by pj.created';
+        where w.id = ? GROUP by l.projectno order by pj.created)
+UNION ALL
+select "","","total","",sum(income), sum(outcome), sum(refund), sum(income - outcome - refund) as balance from
+(select pj.projectno, w.id, pj.name, workgroup, income, outcome, refund, sum(income - outcome + refund) as balance from (
+        (select  projectno, sum(income) as income, sum(outcome) as outcome, sum(refund) as refund  from list GROUP by projectno) as l
+        right JOIN
+        (select * from project) as pj on pj.projectno = l.projectno
+        left JOIN
+        (select * from workgroup) as w on w.name = pj.workgroup)
+        where w.id = ? GROUP by l.projectno order by pj.created) as total';
     $stmt = mysqli_prepare($database, $query);
-    mysqli_stmt_bind_param($stmt, 'i', $_GET['id']);
+    mysqli_stmt_bind_param(
+        $stmt,
+        'ii',
+        $_GET['id'],
+        $_GET['id']
+    );
     mysqli_stmt_execute($stmt);
     $result = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
 
